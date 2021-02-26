2ae68cece0857097589eedf630a4522ab7090037
Matt Flowers <mattmflowers28@gmail.com>
2021-02-26 01:36:34 -0500

    Set up storybook

---
+++ src/components/StateInput.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import { Input, Typography } from 'antd';
+import { CheckCircleTwoTone, CloseCircleTwoTone, LoadingOutlined } from '@ant-design/icons';
+import { red, green } from '@ant-design/colors';
+
+const { Text } = Typography;
+
+function getAddonAfter(state) {
+  if (state === 'CORRECT') {
+      return <CheckCircleTwoTone twoToneColor={green.primary} />;
+  }
+  if (state === 'INCORRECT' || state === 'REPEAT') {
+      return <CloseCircleTwoTone twoToneColor={red.primary} />
+  }
+  if (state === 'TYPING') {
+      return <LoadingOutlined />
+  }
+  return null;
+}
+
+function getMessage(state) {
+  if (state === 'CORRECT') {
+    return <Text type="success">Correct</Text>
+  }
+  if (state === 'INCORRECT') {
+    return <Text type="danger">Not A State</Text>
+  }
+  if (state === 'REPEAT') {
+    return <Text type="danger">Drink</Text>
+  }
+  return null;
+}
+
+const StateInput = ({ id, value, state }) => {
+
+  const addonAfter = getAddonAfter(state);
+  const message = getMessage(state);
+
+  return (
+    <div>
+      <Input value={value} addonBefore={id} addonAfter={addonAfter} />
+      {message}
+    </div>
+  );
+};
+
+StateInput.propTypes = {
+  id: PropTypes.number.isRequired,
+  value: PropTypes.string.isRequired,
+  state: PropTypes.string.isRequired,
+};
+
+export default StateInput;
---
+++ src/components/StateInput.stories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import StateInput from './StateInput';
+
+export default {
+  component: StateInput,
+  title: 'State Input',
+};
+
+const Template = args => <StateInput {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: 1,
+  value: '',
+};
+
+export const Correct = Template.bind({});
+Correct.args = {
+  ...Default.args,
+  state: 'CORRECT',
+};
+
+export const Incorrect = Template.bind({});
+Incorrect.args = {
+  ...Default.args,
+  state: 'INCORRECT',
+};
+
+export const Repeat = Template.bind({});
+Repeat.args = {
+  ...Default.args,
+  state: 'REPEAT',
+};
+
+export const Typing = Template.bind({});
+Typing.args = {
+  ...Default.args,
+  state: 'TYPING',
+};
